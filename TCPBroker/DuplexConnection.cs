b638399a71e09099bc8d08b00fc862fbea00b0dc
Nathan McCann <nmccann@nascar.com>
2017-07-24 13:29:55 -0400

    First working version

---
+++ TCPBroker/DuplexConnection.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPBroker
+{
+
+	public class DuplexConnection
+	{
+		public ReadWriteClient Inbound { get; set; }
+		public ReadWriteClient Outbound { get; set; }
+		public void Start()
+		{
+			Inbound.Start();
+			Outbound.Start();
+		}
+	}
+
+	public class ReadWriteClient
+	{
+
+		public TcpClient Client { get; private set; }
+
+		public NetworkStream Stream { get; private set; }
+
+		public Action OnClose { get; set; }
+
+		public Action<byte[], int> OnRead { get; set; }
+
+		public bool Connected { get; private set; }
+
+		public string Name { get; set; }
+
+		private readonly object _lck = new object();
+
+		public ReadWriteClient(TcpClient client, string name)
+		{
+			Client = client;
+			Stream = client.GetStream();
+			
+			Connected = true;
+			Name = name;
+		}
+
+		public void Send(byte[] buff, int len)
+		{
+			if (Connected && Stream != null && Stream.CanWrite)
+			{
+				Stream.Write(buff, 0, len);
+			}
+		}
+
+		public void Start()
+		{
+			read();
+		}
+
+		public void Stop()
+		{
+			try
+			{
+				if (Client != null) Client.Close();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		private Task read()
+		{
+			return Task.Run(() =>
+			{
+				try
+				{
+					while (Stream.CanRead)
+					{
+						var buff = new byte[Client.ReceiveBufferSize];
+						var len = Stream.Read(buff, 0, buff.Length);
+
+						if (len == 0)
+						{
+							OnClose();
+							Client.Close();
+							break;
+						}
+
+						Console.WriteLine("*** From " + Name + " ***");
+						Console.WriteLine(len + " bytes");
+						Console.WriteLine("***.........................................***");
+
+						OnRead(buff, len);
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+
+				Connected = false;
+			});
+		}
+
+	}
+
+}
--- TCPBroker/Program.cs
+++ TCPBroker/Program.cs
@@ -12,34 +12,77 @@ namespace TCPBroker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TCPBroker");
-
-            System.Threading.Thread.Sleep(5000);
-
-            var srv = new TcpListener(IPAddress.Any, 52002);
-
-            srv.Start();
-
-            Console.WriteLine("Server started");
-
-            while (true)
-            {
-                var client = srv.AcceptTcpClient();
-
-                Console.WriteLine("Client connected");
-
-                var tgClient = new TcpClient();
-
-                //tgClient.Connect("10.220.5.246", 52002);
-                tgClient.Connect("127.0.0.1", 52001);
-
-                Console.WriteLine("TGClient connected");
-
-                var s1 = tgClient.GetStream();
-                var s2 = client.GetStream();
-
-                var stitch = new Stitch(s1, s2);
-            }
+			run();
+			return;
         }
+
+		private static string getLocalIPAddress()
+		{
+			var host = Dns.GetHostEntry(Dns.GetHostName());
+			foreach (var ip in host.AddressList)
+			{
+				if (ip.AddressFamily == AddressFamily.InterNetwork)
+				{
+					return ip.ToString();
+				}
+			}
+			throw new Exception("Local IP Address Not Found!");
+		}
+
+		private static void run()
+		{
+			Console.WriteLine("IP: " + getLocalIPAddress());
+
+			var srv = new TcpListener(IPAddress.Any, 52002);
+			srv.Start();
+			Console.WriteLine("Server started");
+
+			var wrkRecPipe = srv.AcceptTcpClient();
+			Console.WriteLine("Client connected");
+			var wrkSendPipe = new TcpClient();
+			wrkSendPipe.Connect(wrkRecPipe.GetRemoteIP(), 52002);
+			Console.WriteLine("Connected to client");
+
+			var wrkDupe = new DuplexConnection();
+			wrkDupe.Inbound = new ReadWriteClient(wrkRecPipe, "Wrk Inbound");
+			wrkDupe.Outbound = new ReadWriteClient(wrkSendPipe, "Wrk Outbound");
+
+			// connect to TG server
+			var srvSendPipe = new TcpClient();
+			srvSendPipe.Connect("10.220.5.246", 52002);
+			Console.WriteLine("Connected to server");
+
+			// TG server will connect to us
+			var srvRecPipe = srv.AcceptTcpClient();
+			Console.WriteLine("Server connected");
+
+			var srvDupe = new DuplexConnection();
+			srvDupe.Inbound = new ReadWriteClient(srvRecPipe, "Server Inbound");
+			srvDupe.Outbound = new ReadWriteClient(srvSendPipe, "Server Outbound");
+
+			srvDupe.Inbound.OnRead = wrkDupe.Outbound.Send;
+			wrkDupe.Inbound.OnRead = srvDupe.Outbound.Send;
+
+			srvDupe.Inbound.OnClose = srvDupe.Outbound.Stop;
+			wrkDupe.Inbound.OnClose = wrkDupe.Outbound.Stop;
+
+			srvDupe.Start();
+			wrkDupe.Start();
+
+			Console.WriteLine("Press enter to quit");
+			Console.ReadLine();
+		}
+
     }
+
+	public static class extensions
+	{
+		public static IPAddress GetRemoteIP(this TcpClient client)
+		{
+			var ep = client.Client.RemoteEndPoint as System.Net.IPEndPoint;
+
+			return ep.Address;
+		}
+	}
+
 }
